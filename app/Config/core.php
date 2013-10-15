38750d812f272491b17af88d63a18be8569ec355
Raju Sykam <www.raju@gmail.com>
2013-10-14 22:18:52 -0700

    Changed Default Security.salt, Security.cipherSeed and added database.php by renaming database.php.default to database.php

--- app/Config/core.php
+++ app/Config/core.php
@@ -224,12 +224,12 @@
 /**
  * A random string used in security hashing methods.
  */
-	Configure::write('Security.salt', 'DYhG93b0qyJfIxfs2guVoUubWwvniR2G0FgaC9mi');
+	Configure::write('Security.salt', 'GbUrRaUhma45xsJhgntS8juio09D4flmcFtmn7YT');
 
 /**
  * A random numeric string (digits only) used to encrypt/decrypt strings.
  */
-	Configure::write('Security.cipherSeed', '76859309657453542496749683645');
+	Configure::write('Security.cipherSeed', '19621964201319841616050808999');
 
 /**
  * Apply timestamps with the last modified time to static assets (js, css, images).
